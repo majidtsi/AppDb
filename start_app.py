63cb4d29481add5b156d431ffdc8110527ee3427
majid <majidtsi3@gmail.com>
2023-04-26 07:50:29 +0200

    First release of app with db

---
+++ start_app.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from main_window_biblio import MainWindowBiblio
+
+
+app = QApplication(sys.argv)
+
+mainWindowBiblio = MainWindowBiblio()
+mainWindowBiblio.show()
+
+rc = app.exec_()
+sys.exit(rc)
+
+
